cf3e197b71e3d5bcdc7f3df5f8d10deee47c0a73
Dmitry Misharov <quarckster@gmail.com>
2018-08-11 22:30:46 +0200

    Big refactoring

--- video.kino.pub/resources/lib/authwindow.py
+++ video.kino.pub/resources/lib/authwindow.py
@@ -1,31 +1,24 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import xbmcgui, xbmcaddon, xbmc
-import thread
-import threading
-import urllib2
-import urllib
 import json
 import time
+import threading
+import xbmc
+import xbmcgui
+import urllib
+import urllib2
 
-OAUTH_API_URL = "http://api.service-kp.com/oauth2/device"
-CLIENT_ID = "xbmc"
-CLIENT_SECRET = "cgg3gtifu46urtfp2zp1nqtba0k2ezxh"
-
-__id__ = 'video.kino.pub'
-__addon__ = xbmcaddon.Addon(id=__id__)
-__settings__ = xbmcaddon.Addon(id=__id__)
 
 class Auth(object):
     terminated = False
     timer = 0
+    CLIENT_ID = "xbmc"
+    CLIENT_SECRET = "cgg3gtifu46urtfp2zp1nqtba0k2ezxh"
+    OAUTH_API_URL = "http://api.service-kp.com/oauth2/device"
     ERROR, PENDING_STATUS, SUCCESS, EXPIRED = range(4)
 
     def __init__(self, settings, window=None, afterAuth=None):
         self.window = window
-        self.client_id = CLIENT_ID
-        self.client_secret = CLIENT_SECRET
         self.settings = settings
         self.afterAuth = afterAuth
 
@@ -33,33 +26,47 @@ class Auth(object):
         if self.window is not None:
             self.window.close()
 
-    def get_access_token(self):
-        return self.settings.getSetting('access_token')
+    @property
+    def is_token_valid(self):
+        return self.access_token_expire > int(time.time())
 
-    def set_access_token(self, value):
+    @property
+    def access_token(self):
+        return self.settings.getSetting("access_token")
+
+    @access_token.setter
+    def access_token(self, value):
         if value is not None:
-            value = value.encode('utf-8')
-        self.settings.setSetting('access_token', value)
+            value = value.encode("utf-8")
+        self.settings.setSetting("access_token", value)
 
-    def get_refresh_token(self):
-        return self.settings.getSetting('refresh_token')
+    @property
+    def access_token_expire(self):
+        return int(self.settings.getSetting("access_token_expire"))
 
-    def set_refresh_token(self, value):
+    @access_token_expire.setter
+    def access_token_expire(self, value):
         if value is not None:
-            value = value.encode('utf-8')
-        self.settings.setSetting('refresh_token', value)
+            value = value.encode("utf-8")
+        self.settings.setSetting("access_token_expire", value)
 
+    @property
+    def refresh_token(self):
+        return self.settings.getSetting("refresh_token")
 
-    access_token = property(get_access_token, set_access_token)
-    refresh_token = property(get_refresh_token, set_refresh_token)
+    @refresh_token.setter
+    def refresh_token(self, value):
+        if value is not None:
+            value = value.encode("utf-8")
+        self.settings.setSetting("refresh_token", value)
 
     def reauth(self):
         self.access_token = ""
         self.device_token = ""
 
     def request(self, url, data):
-        xbmc.log("REQUEST URL=%s" % url)
-        xbmc.log("DATA %s" % data)
+        xbmc.log("REQUEST URL={}".format(url))
+        xbmc.log("DATA {}".format(data))
         try:
             udata = urllib.urlencode(data)
             req = urllib2.Request(url)
@@ -72,52 +79,51 @@ class Auth(object):
                 try:
                     resp = json.loads(_data)
                     return resp
-                except:
+                except Exception:
                     pass
-            return {'status': e.code, 'error': 'unknown error'}
-        xbmc.executebuiltin("XBMC.Notification(%s,%s)" % ("Internet problems", "Connection timed out!"))
+            return {"status": e.code, "error": "unknown error"}
+        xbmc.executebuiltin("XBMC.Notification(Internet problems,Connection timed out!)")
         if self.window:
             self.window.close()
 
-    def get_device_code(self, url=OAUTH_API_URL):
+    def get_device_code(self):
         data = {
-            'grant_type': 'device_code',
-            'client_id': self.client_id,
-            'client_secret': self.client_secret,
+            "grant_type": "device_code",
+            "client_id": self.CLIENT_ID,
+            "client_secret": self.CLIENT_SECRET
         }
-        resp = self.request(url, data)
-        error = resp.get('error')
+        resp = self.request(self.OAUTH_API_URL, data)
+        error = resp.get("error")
         if error:
-            #xbmc.executebuiltin("XBMC.Notification(%s,%s)" % ("Unknown device", "Unknown device. Please visit http://kino.pub/device/support for more details."))
             return self.ERROR, resp
-        self.device_code = resp['code'].encode('utf-8')
-        self.user_code = resp['user_code'].encode('utf-8')
-        self.verification_uri = resp['verification_uri'].encode('utf8')
-        self.refresh_interval = int(resp['interval'])
+        self.device_code = resp["code"].encode("utf-8")
+        self.user_code = resp["user_code"].encode("utf-8")
+        self.verification_uri = resp["verification_uri"].encode("utf8")
+        self.refresh_interval = int(resp["interval"])
 
-        self.settings.setSetting('device_code', str(self.device_code).encode('utf-8'))
-        self.settings.setSetting('verification_uri', str(self.verification_uri).encode('utf-8'))
-        self.settings.setSetting('interval', str(self.refresh_interval))
+        self.settings.setSetting("device_code", str(self.device_code).encode("utf-8"))
+        self.settings.setSetting("verification_uri", str(self.verification_uri).encode("utf-8"))
+        self.settings.setSetting("interval", str(self.refresh_interval))
         return self.SUCCESS, resp
 
-    def get_token(self, url=OAUTH_API_URL, refresh=False):
+    def get_token(self, refresh=False):
         if refresh:
             data = {
-                'grant_type': 'refresh_token',
-                'refresh_token': self.refresh_token,
-                'client_id': self.client_id,
-                'client_secret': self.client_secret,
+                "grant_type": "refresh_token",
+                "refresh_token": self.refresh_token,
+                "client_id": self.CLIENT_ID,
+                "client_secret": self.CLIENT_SECRET
             }
         else:
             data = {
-                'grant_type': 'device_token',
-                'client_id': self.client_id,
-                'code': self.device_code,
-                'client_secret': self.client_secret,
+                "grant_type": "device_token",
+                "client_id": self.CLIENT_ID,
+                "code": self.device_code,
+                "client_secret": self.CLIENT_SECRET
             }
-        resp = self.request(url, data)
+        resp = self.request(self.OAUTH_API_URL, data)
 
-        error = resp.get('error')
+        error = resp.get("error")
         if error and error == "authorization_pending":
             return self.PENDING_STATUS, resp
         if error and error in ["invalid_grant", "code_expired", "invalid_client"]:
@@ -125,15 +131,15 @@ class Auth(object):
         if error:
             return self.ERROR, resp
 
-        xbmc.log("ERROR IS %s" % error)
-        expires_in = int(resp.get('expires_in')) + int(time.time())
-        self.access_token = resp.get('access_token')
-        self.settings.setSetting('access_token_expire', str(expires_in))
+        xbmc.log("ERROR IS {}".format(error))
+        expires_in = int(resp.get("expires_in")) + int(time.time())
+        self.access_token = resp.get("access_token")
+        self.access_token_expire = str(expires_in)
 
         if self.access_token:
             for key, val in resp.items():
-                self.settings.setSetting(key.encode('utf-8'), str(val).encode('utf-8'))
-            self.settings.setSetting('device_code', '')
+                self.settings.setSetting(key.encode("utf-8"), str(val).encode("utf-8"))
+            self.settings.setSetting("device_code", "")
             return self.SUCCESS, resp
         return self.ERROR, resp
 
@@ -149,27 +155,31 @@ class Auth(object):
 class AuthWindow(xbmcgui.WindowXMLDialog):
     def __init__(self, *args, **kwargs):
         self.stopped = threading.Event()
-        self.auth = Auth(kwargs['settings'], window=self, afterAuth=kwargs['afterAuth'])
+        self.auth = Auth(kwargs["settings"], window=self, afterAuth=kwargs["afterAuth"])
 
     def onInit(self):
         status, resp = self.auth.get_device_code()
         if status == self.auth.ERROR:
             label = self.getControl(9111)
-            label.setLabel('Устройство не поддерживается.\nПосетите http://kino.pub/device/support для уточнения деталей.')
+            label.setLabel(
+                "Устройство не поддерживается.\n"
+                "Посетите http://kino.pub/device/support для уточнения деталей."
+            )
             return
         label = self.getControl(9112)
-        label.setLabel(resp['verification_uri'].encode('utf-8'))
+        label.setLabel(resp["verification_uri"].encode("utf-8"))
         label = self.getControl(9113)
-        label.setLabel(resp['user_code'].encode('utf-8'))
-        t = threading.Thread(target=self.auth.verify_device_code, args=[int(resp['interval']), self])
+        label.setLabel(resp["user_code"].encode("utf-8"))
+        t = threading.Thread(
+            target=self.auth.verify_device_code,
+            args=[int(resp["interval"]), self]
+        )
         t.daemon = True
         t.start()
 
-
     def onAction(self, action):
-        buttonCode =  action.getButtonCode()
-        actionID   =  action.getId()
-        if (actionID in [9, 10, 13, 14, 15]):
+        actionID = action.getId()
+        if actionID in [9, 10, 13, 14, 15]:
             self.closeWindow()
 
     def closeWindow(self):
@@ -178,4 +188,3 @@ class AuthWindow(xbmcgui.WindowXMLDialog):
             if (thread.isAlive() and thread != threading.currentThread()):
                 thread.join(1)
         self.close()
-
