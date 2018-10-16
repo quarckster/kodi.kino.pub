4eb7c237869ff1bf9bbc68ab5447cf8ed24e24b9
Dmitry Misharov <quarckster@gmail.com>
2018-10-16 23:58:43 +0200

    Templatized addon.xml, small improvements of build script

--- build.sh
+++ build.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-VERSION=`git tag -l | tail -n1`
-DIR=video.kino.pub-$VERSION 
+if [[ -d .git ]]; then
+    VERSION=`git tag -l | tail -n1`
+elif [[ $# -eq 0 ]]; then
+    echo "Current directory is not a git repository."
+    echo "Provide a version as an argument."
+    exit 1
+else
+    VERSION=$1
+fi
+
+DIR=video.kino.pub-$VERSION
 
 mkdir $DIR
-cp -r resources addon.xml default.py icon.png $DIR
-zip -r -9 -m $DIR.zip $DIR > /dev/null
+echo "Copying the files to a temporary directory"
+echo "=========================================="
+VERSION=$VERSION envsubst < addon.xml > $DIR/addon.xml
+rsync -rv --exclude=*.pyc resources default.py icon.png $DIR
+echo
+echo "Creating the addon archive"
+echo "=========================="
+zip -rv -9 -m $DIR.zip $DIR
