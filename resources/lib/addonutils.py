51f24d496177694fc1fa4bc8a552c49494501ea1
limeone <limeone244@gmail.com>
2018-12-21 09:55:21 +0200

    remove ratings from plot

--- resources/lib/addonutils.py
+++ resources/lib/addonutils.py
@@ -10,7 +10,7 @@ from functools import wraps
 import xbmc
 import xbmcgui
 
-from data import __id__, __settings__, __plugin__, __extended_plot__, __ratings_source__
+from data import __id__, __settings__, __plugin__, __ratings_source__
 
 
 def dict_merge(old, new):
@@ -57,22 +57,6 @@ def get_mlink(video, stream_type=None, quality=None, ask_quality="false"):
             return files[natural_sort(files.keys())[-1]][stream_type]
 
 
-def build_plot(item):
-    # Don't put rating to plot if user don't want to
-    if __extended_plot__ == 'false':
-        return item["plot"]
-    final_plot = []
-    if item["imdb_rating"]:
-        final_plot.append("IMDB: {}".format(str(round(item["imdb_rating"], 1))))
-    if item["kinopoisk_rating"]:
-        final_plot.append(u"Кинопоиск: {}".format(str(round(item["kinopoisk_rating"], 1))))
-    # a new line between the ratings and the plot
-    if item["imdb_rating"] or item["kinopoisk_rating"]:
-        final_plot.append("")
-    final_plot.append(item["plot"])
-    return "\n".join(final_plot)
-
-
 # Take rating from specified resource if this rating exist
 def build_rating(item):
     rating = item["imdb_rating"] if __ratings_source__ == 'IMDB' else item["kinopoisk_rating"]
@@ -96,7 +80,7 @@ def video_info(item, extend=None):
         "rating": build_rating(item),
         "cast": [x.strip() for x in item["cast"].split(",")],
         "director": item["director"],
-        "plot": build_plot(item),
+        "plot": item["plot"],
         "title": item["title"],
         "duration": item.get("duration", {}).get("average"),
         "imdbnumber": item["imdb"],
