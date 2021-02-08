26d005a05910e88f4c57c894b84c650463c90656
Dmitry Misharov <mail@misharov.pro>
2021-02-08 07:00:53 +0100

    Added integration tests

---
+++ tests/expected_results.py
@@ -0,0 +1,120 @@
+NONACTIVATED_HOME = [
+    {
+        "file": "plugin://video.kino.pub/login/",
+        "filetype": "file",
+        "label": "Активировать устройство",
+        "type": "unknown",
+    }
+]
+
+
+ACTIVATED_HOME = [
+    {
+        "file": "plugin://video.kino.pub/profile/",
+        "filetype": "file",
+        "label": "Профиль",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/search/all/",
+        "filetype": "directory",
+        "label": "Поиск",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/bookmarks/",
+        "filetype": "directory",
+        "label": "Закладки",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/watching/",
+        "filetype": "directory",
+        "label": "Я смотрю",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/watching_movies/",
+        "filetype": "directory",
+        "label": "Недосмотренные",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/items/all/fresh/",
+        "filetype": "directory",
+        "label": "Последние",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/items/all/popular/",
+        "filetype": "directory",
+        "label": "Популярные",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/items/all/hot/",
+        "filetype": "directory",
+        "label": "Горячие",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/items/all/sort/",
+        "filetype": "directory",
+        "label": "По рейтингу по убыванию",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/tv/",
+        "filetype": "directory",
+        "label": "ТВ",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/collections/",
+        "filetype": "directory",
+        "label": "Подборки",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/items/movies/",
+        "filetype": "directory",
+        "label": "Фильмы",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/items/serials/",
+        "filetype": "directory",
+        "label": "Сериалы",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/items/tvshow/",
+        "filetype": "directory",
+        "label": "ТВ шоу",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/items/3d/",
+        "filetype": "directory",
+        "label": "3D",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/items/concerts/",
+        "filetype": "directory",
+        "label": "Концерты",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/items/documovies/",
+        "filetype": "directory",
+        "label": "Документальные фильмы",
+        "type": "unknown",
+    },
+    {
+        "file": "plugin://video.kino.pub/items/docuserials/",
+        "filetype": "directory",
+        "label": "Документальные сериалы",
+        "type": "unknown",
+    },
+]
---
+++ tests/test_integration.py
@@ -0,0 +1,88 @@
+import pytest
+from kodijson import Kodi
+from expected_results import ACTIVATED_HOME, NONACTIVATED_HOME
+from urllib.request import urlopen
+import subprocess
+import time
+import shutil
+from pathlib import Path
+from wait_for import wait_for
+
+
+JSON_RPC_URL = "http://localhost:8080/jsonrpc"
+HOST_DIR = f"{Path('.').absolute()}/tests/data"
+CON_DIR = "/home/kodi/.kodi"
+
+
+@pytest.fixture(scope="session")
+def build_plugin():
+    shutil.copytree("src", f"{HOST_DIR}/addons/video.kino.pub")
+    with open(f"{HOST_DIR}/addons/video.kino.pub/addon.xml", "r+") as addon_xml:
+        content = addon_xml.read()
+        content = content.replace("${VERSION}", "3.99")
+        addon_xml.seek(0)
+        addon_xml.write(content)
+    yield
+    shutil.rmtree(f"{HOST_DIR}/addons/video.kino.pub")
+
+
+@pytest.fixture(scope="session")
+def run_conkodi(build_plugin):
+    container_id = (
+        subprocess.run(
+            [
+                "podman",
+                "run",
+                "--name",
+                "kodi",
+                "-d",
+                "--rm",
+                "-v",
+                f"{HOST_DIR}/addons/:{CON_DIR}/addons",
+                "-v",
+                f"{HOST_DIR}/Database/:{CON_DIR}/userdata/Database",
+                "-v",
+                f"{HOST_DIR}/addon_data/:{CON_DIR}/userdata/addon_data/video.kino.pub/",
+                "-p",
+                "5999:5999",
+                "-p",
+                "8080:8080",
+                "quay.io/quarck/conkodi:19",
+            ],
+            stdout=subprocess.PIPE,
+        )
+        .stdout.decode("utf-8")
+        .strip()
+    )
+    yield
+    subprocess.run(["podman", "rm", "-f", container_id], stdout=subprocess.DEVNULL)
+
+
+@pytest.fixture(scope="session")
+def kodi(run_conkodi):
+    wait_for(lambda: urlopen(JSON_RPC_URL), timeout=10, handle_exception=True)
+    return Kodi(JSON_RPC_URL)
+
+
+def test_home_activated(kodi):
+    resp = kodi.Files.GetDirectory(directory="plugin://video.kino.pub")
+    assert ACTIVATED_HOME == resp["result"]["files"]
+
+
+@pytest.fixture
+def remove_access_token():
+    with open(f"{HOST_DIR}/addon_data/settings.xml", "r+") as settings_xml:
+        orig_content = settings_xml.read()
+        new_content = orig_content.replace(
+            '<setting id="access_token" default="true">some_token</setting>', ""
+        )
+        settings_xml.seek(0)
+        settings_xml.write(new_content)
+    yield
+    with open(f"{HOST_DIR}/addon_data/settings.xml", "w") as settings_xml:
+        settings_xml.write(orig_content)
+
+
+def test_home_nonactivated(kodi, remove_access_token):
+    resp = kodi.Files.GetDirectory(directory="plugin://video.kino.pub")
+    assert NONACTIVATED_HOME == resp["result"]["files"]
