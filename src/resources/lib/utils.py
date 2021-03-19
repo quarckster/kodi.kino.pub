a657fe247b66aa560b574ce2855cc6db6379f1a2
Dmitrii Misharov <quarckster@gmail.com>
2021-03-19 09:52:53 +0100

    Added support of python 3.6 (#186)

--- src/resources/lib/utils.py
+++ src/resources/lib/utils.py
@@ -20,3 +20,18 @@ def fix_m3u8(uri, logger):
         result = f.write(master_playlist.dumps())
     logger.info(f"temporary hls playlist {path} {'was' if result else 'was not'} created")
     return path
+
+
+class cached_property(object):  # noqa
+    """A property that is only computed once per instance and then replaces itself with an ordinary
+    attribute. Deleting the attribute resets the property.
+    """
+
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, obj, cls):
+        if obj is None:
+            return self
+        value = obj.__dict__[self.func.__name__] = self.func(obj)
+        return value
