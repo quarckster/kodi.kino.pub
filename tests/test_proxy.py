b241e3851bee7dbde1a257df0795306db642346e
Dmitry Misharov <mail@misharov.pro>
2024-03-09 17:43:25 +0100

    add more test cases

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -1,29 +1,68 @@
 import re
 import subprocess
+
 import pytest
 
 import expected_results
+from helpers import podman
 
 HTTP_PATTERN = r".*\[http\] 127\.0\.0\.1:\d{5} <-> localhost:1080 via DIRECT"
 SOCKS5_PATTERN = r".*\[socks5\] 127\.0\.0\.1:\d{5} <-> 127.0.0.1:1080 via DIRECT"
 
-@pytest.fixture(scope="module", params=[pytest.param(0, id="http"), pytest.param(3, id="socks5")], autouse=True)
+PARAMS = (
+    pytest.param((0, False, False), id="http-non-auth"),
+    pytest.param((0, True, False), id="http-auth"),
+    pytest.param((0, True, True), id="http-auth-negative"),
+    pytest.param((3, False, False), id="socks5-non-auth"),
+    pytest.param((3, True, False), id="socks5-auth"),
+    pytest.param((3, True, True), id="socks5-auth-negative"),
+)
+
+
+@pytest.fixture(scope="module", params=PARAMS)
 def proxy(kodi, request):
+    proxy_type, is_auth, negative = request.param
+    user = "test_user"
+    password = "test_password"
+    listen_arg = f"{user}:{password}@:8443" if is_auth else ":8443"
+    podman(
+        "run",
+        "--detach",
+        "--pod=kodipod",
+        "--name=proxy",
+        "docker.io/nadoo/glider:0.16",
+        "-listen",
+        listen_arg,
+        "-verbose",
+    )
     kodi.Settings.SetSettingValue(setting="network.usehttpproxy", value=True)
-    kodi.Settings.SetSettingValue(setting="network.httpproxytype", value=request.param)
+    kodi.Settings.SetSettingValue(setting="network.httpproxytype", value=proxy_type)
     kodi.Settings.SetSettingValue(setting="network.httpproxyserver", value="127.0.0.1")
     kodi.Settings.SetSettingValue(setting="network.httpproxyport", value=8443)
-    yield request.param
+    if is_auth:
+        kodi.Settings.SetSettingValue(setting="network.httpproxyusername", value=user)
+        password = "wrong_password" if negative else password
+        kodi.Settings.SetSettingValue(setting="network.httpproxypassword", value=password)
+    yield proxy_type, negative
     kodi.Settings.ResetSettingValue(setting="network.usehttpproxy")
     kodi.Settings.ResetSettingValue(setting="network.httpproxytype")
     kodi.Settings.ResetSettingValue(setting="network.httpproxyserver")
     kodi.Settings.ResetSettingValue(setting="network.httpproxyport")
+    if is_auth:
+        kodi.Settings.ResetSettingValue(setting="network.httpproxyusername")
+        kodi.Settings.ResetSettingValue(setting="network.httpproxypassword")
+    podman("rm", "-f", "proxy")
 
 
-def test_bookmarks_proxy(kodi, proxy):
+def test_proxy(kodi, proxy):
+    proxy_type, negative = proxy
     resp = kodi.Files.GetDirectory(directory="plugin://video.kino.pub/bookmarks/")
-    assert expected_results.BOOKMARKS == resp["result"]["files"]
+    if not negative:
+        assert expected_results.BOOKMARKS == resp["result"]["files"]
     ps = subprocess.run(["podman", "logs", "proxy"], capture_output=True)
-    pattern = HTTP_PATTERN if proxy == 0 else SOCKS5_PATTERN
     last_line = ps.stderr.splitlines()[-1].decode("utf-8")
-    assert re.match(pattern, last_line)
+    if negative:
+        assert "auth failed" in last_line
+    else:
+        pattern = HTTP_PATTERN if proxy_type == 0 else SOCKS5_PATTERN
+        assert re.match(pattern, last_line)
